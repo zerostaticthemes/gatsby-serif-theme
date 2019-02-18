44ac7fa42838226d50def63317d1b3e6a38a655a
Robert Austin <rob@jugglerdigital.com>
2019-02-19 09:32:50 +1000

    add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const guid = process.env.NETLIFY_GOOGLE_ANALYTICS_ID;
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Serif',
@@ -49,6 +51,14 @@ module.exports = {
       options: {
         path: `${__dirname}/src/images`,
         name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: guid ? guid : 'UA-XXX-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
       },
     },
   ],
