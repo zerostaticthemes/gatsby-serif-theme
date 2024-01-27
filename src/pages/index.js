0c5e96d72cb5512cc4cf7969a0a0568a1d110ecf
Troy Kohler <troy.kohler@zalando.de>
2024-01-27 17:54:15 +0100

    review index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+// reviewed;
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Helmet from 'react-helmet';
@@ -6,11 +7,15 @@ import Layout from '../components/Layout';
 import Call from '../components/Call';
 
 const Home = props => {
-  const intro = props.data.intro;
+  const intro = props.data.intro; // { html, frontmatter: { title, image } }
   const site = props.data.site.siteMetadata;
   const services = props.data.services.edges;
   const features = props.data.features.edges;
-  const introImageClasses = `intro-image ${intro.frontmatter.intro_image_absolute && 'intro-image-absolute'} ${intro.frontmatter.intro_image_hide_on_mobile && 'intro-image-hide-mobile'}`;
+  const introImageClasses = `intro-image ${
+    intro.frontmatter.intro_image_absolute && 'intro-image-absolute'
+  } ${
+    intro.frontmatter.intro_image_hide_on_mobile && 'intro-image-hide-mobile'
+  }`;
 
   return (
     <Layout bodyClass="page-home">
@@ -18,7 +23,7 @@ const Home = props => {
       <Helmet>
         <meta
           name="description"
-          content="Small Business Theme. Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This is a beautiful and artfully designed starting theme."
+          content="Small Business Theme. Multiple content types using Markdown and JSON sources. Responsive design and SCSS. This is a beautiful and artfully designed starting theme." // FIXME: Update description
         />
       </Helmet>
 
@@ -31,13 +36,17 @@ const Home = props => {
             </div>
             {intro.frontmatter.intro_image && (
               <div className="col-12 col-md-5 col-lg-6 order-1 order-md-2 position-relative">
-                <img alt={intro.frontmatter.title} className={introImageClasses} src={intro.frontmatter.intro_image} />
+                <img
+                  alt={intro.frontmatter.title}
+                  className={introImageClasses}
+                  src={intro.frontmatter.intro_image}
+                />
               </div>
             )}
           </div>
         </div>
       </div>
-
+      {/* services */}
       {services.length > 0 && (
         <div className="strip">
           <div className="container pt-6 pb-6 pb-md-10">
@@ -47,7 +56,9 @@ const Home = props => {
                   <div className="service service-summary">
                     <div className="service-content">
                       <h2 className="service-title">
-                        <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+                        <Link to={node.fields.slug}>
+                          {node.frontmatter.title}
+                        </Link>
                       </h2>
                       <p>{node.excerpt}</p>
                     </div>
@@ -57,13 +68,15 @@ const Home = props => {
             </div>
             <div className="row justify-content-center">
               <div className="col-auto">
-                <Link className="button button-primary" to="/services/">View All Services</Link>
+                <Link className="button button-primary" to="/services/">
+                  View All Services
+                </Link>
               </div>
             </div>
           </div>
         </div>
       )}
-
+      {/* features */}
       {features.length > 0 && (
         <div className="strip strip-grey">
           <div className="container pt-6 pb-6 pt-md-10 pb-md-10">
@@ -92,7 +105,7 @@ const Home = props => {
 export const query = graphql`
   query {
     services: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/services\/.*/" } }
+      filter: { fileAbsolutePath: { regex: "/services/.*/" } }
       sort: { fields: [frontmatter___weight], order: ASC }
       limit: 6
     ) {
@@ -110,17 +123,15 @@ export const query = graphql`
         }
       }
     }
-    intro: markdownRemark(
-      fileAbsolutePath: {regex: "/content/index.md/"}
-    ) {
-        html
-        frontmatter {
-          image
-          intro_image
-          intro_image_absolute
-          intro_image_hide_on_mobile
-          title
-        }
+    intro: markdownRemark(fileAbsolutePath: { regex: "/content/index.md/" }) {
+      html
+      frontmatter {
+        image
+        intro_image
+        intro_image_absolute
+        intro_image_hide_on_mobile
+        title
+      }
     }
     features: allFeaturesJson {
       edges {
